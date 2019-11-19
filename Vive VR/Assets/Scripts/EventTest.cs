12d7ece74001075d8629a49c6731eb6f92518574
jsantana29 <santanarijo29@gmail.com>
2019-11-18 23:58:58 -0500

    Easter egg
    Added something very necessary

---
+++ Vive VR/Assets/Scripts/EventTest.cs
@@ -0,0 +1,56 @@
+﻿//This Script is just for testing the EventSystem
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour
+{
+    public int id = 69;
+    public int id2 = 420;
+
+    public Transform girl;
+    public Transform ball;
+
+    private AudioSource audio;
+
+
+
+    void Start()
+    {
+        EventSystem.eventController.onTestEventTrigger += TestEvent;
+        EventSystem.eventController.onTestEventTrigger += TestMultiEvent;
+        EventSystem.eventController.onTestEventTrigger += TestMultiEventSecond;
+
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    private void TestEvent(int id)
+    {
+        if (id == this.id)
+        {
+            Debug.Log("Successful Test ID: " + id);
+            girl.position = ball.position;
+            audio.Play();
+
+        }
+    }
+
+    private void TestMultiEvent(int id) 
+    {
+        if (id == id2) 
+        {
+            Debug.Log("Successful Test!!!");
+            Debug.Log(id2 + "!!!!");
+        }
+    }
+
+    private void TestMultiEventSecond(int id)
+    {
+        if (id == id2)
+        {
+            Debug.Log("BLAZE IT!!!!!!!");
+        }
+    }
+}
